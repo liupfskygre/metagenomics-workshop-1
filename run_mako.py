ed6ed0413ab79ca54f21853e6f02247a8190646d
Johannes Alneberg <johannesalneberg@gmail.com>
2014-11-03 11:06:45 +0100

    Made run mako script more flexible

--- run_mako.py
+++ run_mako.py
@@ -5,27 +5,21 @@ from mako.lookup import TemplateLookup
 import yaml
 import argparse
 import os
-
-FILE_PATH = os.path.dirname(__file__)
-
-TEMPLATES = ['binning/index.rst',
-             'binning/setup.rst',
-             'binning/concoct.rst',
-             'binning/phylosift.rst']
+import glob
 
 def main(args):
-    with open(os.path.join(args.input_path, "settings.yaml")) as settings_file:
+    with open(args.config) as settings_file:
         settings = yaml.load(settings_file)
 
     lookup = TemplateLookup(directories=[args.template_path])
-    for t in TEMPLATES: 
-        template = lookup.get_template(t)
+    for t in glob.glob(os.path.join(args.template_path, "*.rst")): 
+        template = Template(filename=t)
+        template_filename = os.path.basename(t)
         print(template.render(**settings), 
-                file=open(os.path.join(args.output_path, t), 'w'))
+                file=open(os.path.join(args.output_path, template_filename), 'w'))
 
 def sanitize_input(args):
     assert os.path.isdir(args.output_path)
-    assert os.path.isdir(args.input_path)
     assert os.path.isdir(args.template_path)
 
 if __name__ == '__main__':
@@ -34,14 +28,10 @@ if __name__ == '__main__':
         help=('Path to where rendered templates will be written.'
             ' Any existing files with the same file names will '
             'be over written. Default = sources'))
-    parser.add_argument('-i', '--input_path', default='mako/',
-        help=('Path where the raw mako settings '
-            'files are stored. Default = mako'))
     parser.add_argument('-t', '--template_path', default=None,
-        help=('Path where the raw mako templates are stored. '
-            'Default = <input_path>/templates'))
+        help=('Path where the raw mako templates are stored. '))
+    parser.add_argument('--config',
+            help="The yaml file containing config variables for templates.")
     args = parser.parse_args()
-    if args.template_path is None:
-        args.template_path = os.path.join(args.input_path, 'templates')
     sanitize_input(args)
     main(args)
