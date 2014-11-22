cf0b3c799215450ff79e60eee79296b8a91832f2
Johannes Alneberg <johannesalneberg@gmail.com>
2014-11-22 12:44:55 +0100

    Changed names in copyright

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Metagenomics Workshop SciLifeLab'
-copyright = u'2014, Johannes Alneberg, Johan Bengtsson-Palme, Ino de Bruijn, Luisa Hugerth, Mikael Huss, Thomas Svensson'
+copyright = u'2014, Johannes Alneberg, John Larsson, Ino de Bruijn, Luisa Hugerth, Anders Andersson'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
